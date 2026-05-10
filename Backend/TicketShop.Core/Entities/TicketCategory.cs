9486531fe2719639e62897cc439c2889eaab9cec
Samuele Colacino <samu.colacino@gmail.com>
2026-05-10 22:28:21 +0200

    Add TicketCategory entity

---
+++ Backend/TicketShop.Core/Entities/TicketCategory.cs
@@ -0,0 +1,8 @@
+namespace TicketShop.Core.Entities;
+
+public class TicketCategory
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
