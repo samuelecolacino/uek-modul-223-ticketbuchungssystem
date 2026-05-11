cea53066f2f7e42c91d0d1399e9b730d4f62dedc
Samuele Colacino <samu.colacino@gmail.com>
2026-05-11 08:31:02 +0200

    Add TicketService for available and buy

---
+++ Frontend/src/app/tickets/ticket.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface AvailableCategory {
+  categoryId: number;
+  name: string;
+  price: number;
+  availableCount: number;
+  ticketIds: number[];
+}
+
+export interface BuyTicketResponse {
+  ticketId: number;
+  userId: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class TicketService {
+  private readonly http = inject(HttpClient);
+
+  getAvailable(): Observable<AvailableCategory[]> {
+    return this.http.get<AvailableCategory[]>(`${environment.apiUrl}/tickets/available`);
+  }
+
+  buy(ticketId: number): Observable<BuyTicketResponse> {
+    return this.http.post<BuyTicketResponse>(`${environment.apiUrl}/tickets/buy`, { ticketId });
+  }
+}
